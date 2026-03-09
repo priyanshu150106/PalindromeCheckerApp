4046ed96c661b19145a67c10489f95cf0485b54b
priyanshu150106 <priyanshu150106@gmail.com>
2026-03-09 15:16:23 +0530

    UC9: Recursive palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,42 +1,31 @@
-import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
-
 public class PalindromeCheckerApp {
+
+    // Recursive method
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker Application");
+        System.out.println("Welcome to Palindrome Checker App");
 
-        Scanner scanner = new Scanner(System.in);
+        String word = "madam";
 
-        System.out.print("Enter a word: ");
-        String input = scanner.nextLine();
+        boolean result = isPalindrome(word, 0, word.length() - 1);
 
-        Deque<Character> deque = new ArrayDeque<>();
-
-        // Add all characters to deque
-        for (char ch : input.toCharArray()) {
-            deque.addLast(ch);  // Insert at rear
-        }
-
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();  // Remove from front
-            char rear = deque.removeLast();    // Remove from rear
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome");
+        if (result) {
+            System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println(input + " is not a Palindrome");
+            System.out.println(word + " is not a Palindrome");
         }
-
-        scanner.close();
     }
 }
